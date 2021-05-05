50654cefc68f78670ff7b6249c09cb90fe6af092
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-05-05 01:25:52 -0300

    Listagem de usuários

---
+++ src/app/privado/modulos/usuario/servicos/usuario.service.ts
@@ -0,0 +1,18 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  private urlBase = environment.urlBase;
+
+  constructor(private http: HttpClient) {}
+
+  buscarUsuarios(): Observable<any> {
+    return this.http.get<any>(this.urlBase + '/users');
+  }
+}
