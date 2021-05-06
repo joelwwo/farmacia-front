81c4e2c6f313eea834e704e8750a1949e5e998b4
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-05-06 20:39:40 -0300

    Correção das rotas

--- src/app/privado/privado-routing.module.ts
+++ src/app/privado/privado-routing.module.ts
@@ -1,21 +1,30 @@
+import { PrivadoComponent } from './privado.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'usuarios',
-    loadChildren: () =>
-      import('./modulos/usuario/usuario.module').then((m) => m.UsuarioModule),
-  },
-  {
-    path: 'vendas',
-    loadChildren: () =>
-      import('./modulos/vendas/vendas.module').then((m) => m.VendasModule),
-  },
-  {
-    path: '**',
-    redirectTo: 'usuarios',
-    pathMatch: 'full',
+    path: '',
+    component: PrivadoComponent,
+    children: [
+      {
+        path: 'usuarios',
+        loadChildren: () =>
+          import('./modulos/usuario/usuario.module').then(
+            (m) => m.UsuarioModule
+          ),
+      },
+      {
+        path: 'vendas',
+        loadChildren: () =>
+          import('./modulos/vendas/vendas.module').then((m) => m.VendasModule),
+      },
+      {
+        path: '**',
+        redirectTo: 'usuarios',
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
 
