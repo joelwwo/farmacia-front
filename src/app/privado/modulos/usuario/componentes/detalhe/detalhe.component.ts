eb72539607f35343bad7e0ca9c4dce742b54f957
Joel dos Santos Silva <joeldossantossilva21@gmail.com>
2021-05-08 02:26:07 -0300

    Adição de componete e listagem e mapeamento de dados

--- src/app/privado/modulos/usuario/componentes/detalhe/detalhe.component.ts
+++ src/app/privado/modulos/usuario/componentes/detalhe/detalhe.component.ts
@@ -1,15 +1,32 @@
+import { IUsuario } from './../../../../../core/Models/Usuario';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
+import { UsuarioService } from './../../servicos/usuario.service';
+import { EnderecoService } from './../../servicos/endereco/endereco.service';
 
 @Component({
   selector: 'app-detalhe',
   templateUrl: './detalhe.component.html',
-  styleUrls: ['./detalhe.component.styl']
+  styleUrls: ['./detalhe.component.styl'],
 })
 export class DetalheComponent implements OnInit {
+  usuario!: IUsuario;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private EnderecoService: EnderecoService,
+    private usuarioService: UsuarioService
+  ) {}
 
   ngOnInit(): void {
+    this.buscarUsuario();
   }
 
+  buscarUsuario(): void {
+    const id = this.route.snapshot.paramMap.get('id') || '0';
+    this.usuarioService
+      .buscarUsuario(id)
+      .subscribe((usuario) => (this.usuario = usuario));
+  }
 }
--- src/app/privado/modulos/usuario/componentes/endereco/endereco.component.ts
+++ src/app/privado/modulos/usuario/componentes/endereco/endereco.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { IEndereco } from './../../../../../core/Models/Endereco';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-endereco',
   templateUrl: './endereco.component.html',
-  styleUrls: ['./endereco.component.styl']
+  styleUrls: ['./endereco.component.styl'],
 })
 export class EnderecoComponent implements OnInit {
+  @Input() endereco!: IEndereco;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
